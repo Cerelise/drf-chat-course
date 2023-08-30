a5720c6fa4303e960a966084ad4ea7c0b3d6d06b
cerelise <291244924@qq.com>
2023-08-31 01:11:51 +0800

    get message_list from pinia

--- client/src/stores/room.js
+++ client/src/stores/room.js
@@ -1,29 +1,32 @@
-import { defineStore } from 'pinia'
-import useAxios from '../composables/useAxios'
+import { defineStore } from "pinia";
+import useAxios from "../composables/useAxios";
 
-const axios = useAxios()
+const axios = useAxios();
 
 export const useRoomStore = defineStore({
-	id: 'room',
+	id: "room",
 
 	state: () => ({
 		room: {
 			name: null,
 			slug: null,
+			message_list: [],
 		},
 	}),
 
 	actions: {
 		async initStore(room_slug) {
-			console.log('initStore')
-			const token = localStorage.getItem('user.token')
+			console.log("initStore");
+			const token = localStorage.getItem("user.token");
 			if (token) {
 				await axios.get(`/chat/${room_slug}/`).then((response) => {
-					console.log('response :>> ', response)
-					this.room.name = response.room.name
-					this.room.slug = response.room.slug
-				})
+					console.log("response :>> ", response);
+					this.room.name = response.room.name;
+					this.room.slug = response.room.slug;
+					this.room.message_list = response.message_list;
+					console.log(this.room.message_list);
+				});
 			}
 		},
 	},
-})
+});
--- client/src/views/ChatView.vue
+++ client/src/views/ChatView.vue
@@ -1,16 +1,19 @@
 <template>
 	<main>
 		<div class="p-10 text-center lg:p-20">
-			<h1 class="text-3xl text-white lg:text-6xl">{{ roomStore.room.name }}</h1>
+			<h1 class="text-3xl text-white lg:text-6xl">{{ room.name }}</h1>
 		</div>
 
 		<div class="mx-4 p-4 bg-white rounded-xl lg:w-2/4 lg:mx-auto">
 			<div
 				class="chat-messages space-y-3 mb-3"
-				id="chat-messages"
+				id="chat-container"
 				ref="chatContainer"
 			>
-				<div v-for="msg in msgList" class="p-4 bg-gray-200 rounded-xl">
+				<div
+					v-for="msg in room.message_list"
+					class="p-4 bg-gray-200 rounded-xl"
+				>
 					<p class="font-semibold">{{ msg.username }}</p>
 					<p>{{ msg.content }}</p>
 				</div>
@@ -37,44 +40,46 @@
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router'
-import { useUserStore } from '../stores/user'
-import { useRoomStore } from '../stores/room'
-import { onMounted, ref, nextTick } from 'vue'
-import useAxios from '../composables/useAxios'
+import { useRoute } from "vue-router";
+import { useUserStore } from "../stores/user";
+import { useRoomStore } from "../stores/room";
+import { storeToRefs } from "pinia";
+import { onMounted, ref, nextTick } from "vue";
 
-const userStore = useUserStore()
-const roomStore = useRoomStore()
-const route = useRoute()
-const axios = useAxios()
+const userStore = useUserStore();
+const roomStore = useRoomStore();
+const { room } = storeToRefs(useRoomStore());
+const route = useRoute();
 
-const chatSocket = ref('')
-const chatMsg = ref('')
-const msgList = ref([])
+const chatSocket = ref("");
+const chatMsg = ref("");
 
-const chatContainer = ref(null)
+const chatContainer = ref(null);
 
-const room_slug = route.params.room_slug
-roomStore.initStore(room_slug)
+const room_slug = route.params.room_slug;
 
 function initChatSocket() {
-	chatSocket.value = new WebSocket('ws://127.0.0.1:8000/ws/' + room_slug + '/')
+	chatSocket.value = new WebSocket("ws://127.0.0.1:8000/ws/" + room_slug + "/");
 
 	chatSocket.value.onmessage = function (e) {
-		console.log('onmessage')
-		const msg = JSON.parse(e.data)
+		console.log("onmessage");
+		const msg = JSON.parse(e.data);
 		if (msg.content) {
-			msgList.value.push(msg)
-			scrollToButtom()
+			let html = `<div class="p-4 bg-gray-200 rounded-xl">
+			<p class="font-semibold">${msg.username}</p>
+			<p>${msg.content}</p>
+		  </div>`;
+			chatContainer.value.innerHTML += html;
+			scrollToButtom();
 		} else {
-			alert('消息为空！')
-			return
+			alert("消息为空！");
+			return;
 		}
-	}
+	};
 
 	chatSocket.value.onclose = function (e) {
-		console.log('onclose')
-	}
+		console.log("onclose");
+	};
 }
 
 function sendMessage() {
@@ -82,26 +87,24 @@ function sendMessage() {
 		content: chatMsg.value,
 		username: userStore.user.name,
 		room: roomStore.room.slug,
-	})
-	chatSocket.value.send(msg)
+	});
+	chatSocket.value.send(msg);
 
-	chatMsg.value = ''
+	chatMsg.value = "";
 }
 
 function scrollToButtom() {
 	nextTick(() => {
 		if (chatContainer.value) {
-			chatContainer.value.scrollTop = chatContainer.value.scrollHeight
+			chatContainer.value.scrollTop = chatContainer.value.scrollHeight;
 		}
-	})
+	});
 }
 
-onMounted(async () => {
-	// 获取当前聊天室详情
-	await axios.get(`/chat/${route.params.room_slug}/`).then((response) => {
-		msgList.value = response.message_list
-	})
-	scrollToButtom()
-	initChatSocket()
-})
+onMounted(() => {
+	chatContainer.value = document.getElementById("chat-container");
+	roomStore.initStore(room_slug);
+	scrollToButtom();
+	initChatSocket();
+});
 </script>
